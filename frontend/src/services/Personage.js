6e41a82ba481c26fee233389474598afb69a226a
vassili45 <vassili.papaodpoulos45@gmail.com>
2022-11-24 16:33:04 +0100

    modify personage

--- frontend/src/services/Personage.js
+++ frontend/src/services/Personage.js
@@ -1,40 +1,45 @@
 const personages = [
- {
-  personage_id: 1,
-  name:"Albus Gorski",
-  src: "",
-  quote1: "Salut les BG! Vous êtres prêts pour devenir un aussi bon magicien du web que moi?",
- },
- {
-  personage_id: 2,
-  name:"Rogue Richard",
-  src: "",
-  quote1: "Vous ne savez vraiment pas utilisez vos API. Décevant... Essayez plutôt Axios API la prochaine fois comme sortilège!",
- },
- {
-  personage_id: 3,
-  name:"J-F Lupin",
-  src: "",
-  quote1: "Pour combattre les bases de données, il vous faudra un sortilège puissant! Le sortie du Phptronus. Repetez après moi : Expecto Phptronum.",
- },
- {
-  personage_id: 4,
-  name:"Benoîgrid",
-  src: "",
-  quote1: "Grosso modo, il faut arriver à dompter le grid pour votre futur deweb-sorcier / web-developper / developper-sorcier.",
- },
- {
-  personage_id: 5,
-  name:"Kévin Prefet",
-  src: "",
-  quote1: "Le mot de passe est Caput Devonis!",
- },
- {
-  personage_id: 6,
-  name:"Lou-Anne McGonnagal",
-  src: "",
-  quote1: "Aujourd’hui, nous allons apprendre la métamorphosedes interfaces graphiques!",
- }
+  {
+    personage_id: 1,
+    name: "Albus Gorski",
+    src: "./assets/Albus_Gorski.png",
+    quote1:
+      "Salut les BG! Vous êtres prêts pour devenir un aussi bon magicien du web que moi?",
+  },
+  {
+    personage_id: 2,
+    name: "Rogue Richard",
+    src: "./assets/Rogue_Richard.png",
+    quote1:
+      "Vous ne savez vraiment pas utilisez vos API. Décevant... Essayez plutôt Axios API la prochaine fois comme sortilège!",
+  },
+  {
+    personage_id: 3,
+    name: "J-F Lupin",
+    src: "./assets/JF_Lupin.png",
+    quote1:
+      "Pour combattre les bases de données, il vous faudra un sortilège puissant! Le sortie du Phptronus. Repetez après moi : Expecto Phptronum.",
+  },
+  {
+    personage_id: 4,
+    name: "Benoîgrid",
+    src: "./assets/Benoit_Grid.png",
+    quote1:
+      "Grosso modo, il faut arriver à dompter le grid pour votre futur deweb-sorcier / web-developper / developper-sorcier.",
+  },
+  {
+    personage_id: 5,
+    name: "Kévin Prefet",
+    src: "./assets/Kevin_Prefet.png",
+    quote1: "Le mot de passe est Caput Devonis!",
+  },
+  {
+    personage_id: 6,
+    name: "Lou-Anne McGonnagal",
+    src: "./assets/Lou-Anne_McGonagall.png",
+    quote1:
+      "Aujourd’hui, nous allons apprendre la métamorphosedes interfaces graphiques!",
+  },
 ];
 
 export default personages;
