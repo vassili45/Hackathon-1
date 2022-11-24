7637d95465b97047882d2c0ae4fb7bd0e8980baa
Laynaria <pro.laynaria@outlook.fr>
2022-11-24 23:39:27 +0100

    Typo fix on personnage.js

--- frontend/src/services/Personage.js
+++ frontend/src/services/Personage.js
@@ -30,7 +30,7 @@ const personages = [
   {
     personage_id: 4,
     name: "Benoîgrid",
-    src: "./assets/Benoit_Grid.png",
+    src: benoitGrid,
     quote1:
       "Grosso modo, il faut arriver à dompter le grid pour votre futur deweb-sorcier / web-developper / developper-sorcier.",
   },
