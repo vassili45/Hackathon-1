7ecc658c46f72600106ca30fbde77beac71b5630
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2022-11-24 17:19:45 +0100

    ajout du design de la page home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,9 +1,15 @@
-
+import img from "../assets/logo.png"
+import "./Home.css"
 
 export default function Home() {
   return (
-    <div>
-      <h1>Voyage a Poudlard</h1>
+    <div className="mainHomeDiv d-flex flex-column justify-content-center p-0 m-0">
+      <div className="HomeText text-center w-75 align-self-center">
+        <p className="fw-bold">Bienvenue apprenti sorcier.</p>
+        <p className="fw-bold">Ici commence ton aventure à travers la découverte de la Wizard Code School.</p>
+      </div>
+      <button className="HomeButton align-self-end fw-bold" type="button">Continuer</button>
+
     </div>
   );
 }
