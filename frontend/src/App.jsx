5ef492e5b60fc17c9dd1e4a2cabe39463e738d3b
MartinezEmmanuel03 <e.martinez03@reseau.sncf.fr>
2022-11-24 13:42:34 +0100

    mise en place des routes dans app.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,21 @@
 import Home from "./pages/Home";
+import { Router, Routes, Route, Link } from "react-router-dom";
 
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <p>coucou</p>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/lieux" element={<Lieux />} />
+          <Route path="/lieux/:id" element={<Lieu />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
 export default App;
+
