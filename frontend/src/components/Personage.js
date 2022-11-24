8df7a976c2c6b57fe815633b0dc49d9f39d57e4b
vassili45 <vassili.papaodpoulos45@gmail.com>
2022-11-24 14:41:23 +0100

    add personages on compoenets

---
+++ frontend/src/components/Personage.js
@@ -0,0 +1,40 @@
+const personages = [
+ {
+  personage_id: 1,
+  name:"Albus Gorski",
+  src: "",
+  quote1: "Salut les BG! Vous êtres prêts pour devenir un aussi bon magicien du web que moi?",
+ },
+ {
+  personage_id: 2,
+  name:"Rogue Richard",
+  src: "",
+  quote1: "Vous ne savez vraiment pas utilisez vos API. Décevant... Essayez plutôt Axios API la prochaine fois comme sortilège!",
+ },
+ {
+  personage_id: 3,
+  name:"J-F Lupin",
+  src: "",
+  quote1: "Pour combattre les bases de données, il vous faudra un sortilège puissant! Le sortie du Phptronus. Repetez après moi : Expecto Phptronum.",
+ },
+ {
+  personage_id: 4,
+  name:"Benoîgrid",
+  src: "",
+  quote1: "Grosso modo, il faut arriver à dompter le grid pour votre futur deweb-sorcier / web-developper / developper-sorcier.",
+ },
+ {
+  personage_id: 5,
+  name:"Kévin Prefet",
+  src: "",
+  quote1: "Le mot de passe est Caput Devonis!",
+ },
+ {
+  personage_id: 6,
+  name:"Lou-Anne McGonnagal",
+  src: "",
+  quote1: "Aujourd’hui, nous allons apprendre la métamorphosedes interfaces graphiques!",
+ }
+];
+
+export default personages;
