3a6d9fcf082e3a4c3f5e99bce4e43a89d2ce5dc2
Laynaria <pro.laynaria@outlook.fr>
2022-11-24 23:35:04 +0100

    css hotfix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import logo from "@assets/logo.png";
 import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
-
 import Home from "@pages/Home";
 import Lieux from "@pages/Lieux";
 import Lieu from "@pages/Lieu";
@@ -24,8 +23,19 @@ function App() {
         <img src={logo} alt="logo" className="logo" />
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/Lieux" element={<Lieux handleLocation={handleLocation} handleCharacter={handleCharacter} />} />
-          <Route path="/Lieux/:id" element={<Lieu location={location} character={character} />} />
+          <Route
+            path="/Lieux"
+            element={
+              <Lieux
+                handleLocation={handleLocation}
+                handleCharacter={handleCharacter}
+              />
+            }
+          />
+          <Route
+            path="/Lieux/:id"
+            element={<Lieu location={location} character={character} />}
+          />
         </Routes>
       </div>
     </Router>
--- frontend/src/pages/Lieux.jsx
+++ frontend/src/pages/Lieux.jsx
@@ -9,7 +9,6 @@ import lieu from "@services/Lieu";
 import "./LieuxStyle.css";
 
 function Lieux(props) {
-
   const { handleCharacter, handleLocation } = props;
   const [grandeSalleIsShown, setGrandeSalleIsShown] = useState(false);
 
@@ -32,147 +31,186 @@ function Lieux(props) {
   const [sauleIsShown, setSauleIsShown] = useState(false);
 
   return (
-
     <>
       <div className="backgroundMap" style={{ backgroundImage: `url(${Map})` }}>
         {" "}
       </div>
-    <div className="map" style={{ backgroundImage: `url(${Map})` }}>
-      <div className="GrandeSalle">
-        <Link to={`/Lieux/1`}>
-          <input
-            type="button"
-            className="buttGrandeSalle"
-            onMouseEnter={() => setGrandeSalleIsShown(true)}
-            onMouseLeave={() => setGrandeSalleIsShown(false)}
-            onClick={() => { handleCharacter(personages[0]); handleLocation(lieu[0]); }}
-          /></Link>
-        <span className={grandeSalleIsShown ? "isShown" : "isHidden"}>
-          Grande Salle
-        </span>
-      </div>
+      <div className="map" style={{ backgroundImage: `url(${Map})` }}>
+        <div className="GrandeSalle">
+          <Link to="/Lieux/1">
+            <input
+              type="button"
+              className="buttGrandeSalle"
+              onMouseEnter={() => setGrandeSalleIsShown(true)}
+              onMouseLeave={() => setGrandeSalleIsShown(false)}
+              onClick={() => {
+                handleCharacter(personages[0]);
+                handleLocation(lieu[0]);
+              }}
+            />
+          </Link>
+          <span className={grandeSalleIsShown ? "isShown" : "isHidden"}>
+            Grande Salle
+          </span>
+        </div>
 
-      <div className="Dortoir">
-        <Link to={`/Lieux/2`}>
-          <input
-            type="button"
-            className="buttDortoir"
-            onMouseEnter={() => setDortoirIsShown(true)}
-            onMouseLeave={() => setDortoirIsShown(false)}
-            onClick={() => { handleCharacter(personages[4]); handleLocation(lieu[2]); }}
-          /></Link>
+        <div className="Dortoir">
+          <Link to="/Lieux/2">
+            <input
+              type="button"
+              className="buttDortoir"
+              onMouseEnter={() => setDortoirIsShown(true)}
+              onMouseLeave={() => setDortoirIsShown(false)}
+              onClick={() => {
+                handleCharacter(personages[4]);
+                handleLocation(lieu[2]);
+              }}
+            />
+          </Link>
 
-        <span className={dortoirIsShown ? "isShown" : "isHidden"}>Dortoir</span>
-      </div>
+          <span className={dortoirIsShown ? "isShown" : "isHidden"}>
+            Dortoir
+          </span>
+        </div>
 
-      <div className="Hutte">
-        <Link to={`/Lieux/3`}>
-          <input
-            type="button"
-            className="buttHutte"
-            onMouseEnter={() => setHutteIsShown(true)}
-            onMouseLeave={() => setHutteIsShown(false)}
-            onClick={() => { handleCharacter(personages[3]); handleLocation(lieu[1]); }}
-          /></Link>
-        <span className={hutteIsShown ? "isShown" : "isHidden"}>Hutte</span>
-      </div>
+        <div className="Hutte">
+          <Link to="/Lieux/3">
+            <input
+              type="button"
+              className="buttHutte"
+              onMouseEnter={() => setHutteIsShown(true)}
+              onMouseLeave={() => setHutteIsShown(false)}
+              onClick={() => {
+                handleCharacter(personages[3]);
+                handleLocation(lieu[1]);
+              }}
+            />
+          </Link>
+          <span className={hutteIsShown ? "isShown" : "isHidden"}>Hutte</span>
+        </div>
 
-      <div className="Potion">
-        <Link to={`/Lieux/4`}>
-          <input
-            type="button"
-            className="buttPotion"
-            onMouseEnter={() => setPotionIsShown(true)}
-            onMouseLeave={() => setPotionIsShown(false)}
-            onClick={() => { handleCharacter(personages[1]); handleLocation(lieu[4]); }}
-          /></Link>
-        <span className={potionIsShown ? "isShown" : "isHidden"}>
-          Salle des Potions
-        </span>
-      </div>
+        <div className="Potion">
+          <Link to="/Lieux/4">
+            <input
+              type="button"
+              className="buttPotion"
+              onMouseEnter={() => setPotionIsShown(true)}
+              onMouseLeave={() => setPotionIsShown(false)}
+              onClick={() => {
+                handleCharacter(personages[1]);
+                handleLocation(lieu[4]);
+              }}
+            />
+          </Link>
+          <span className={potionIsShown ? "isShown" : "isHidden"}>
+            Salle des Potions
+          </span>
+        </div>
 
-      <div className="Meta">
-        <Link to={`/Lieux/5`}>
-          <input
-            type="button"
-            className="buttMeta"
-            onMouseEnter={() => setMetaIsShown(true)}
-            onMouseLeave={() => setMetaIsShown(false)}
-            onClick={() => { handleCharacter(personages[5]); handleLocation(lieu[5]); }}
-          /></Link>
-        <span className={metaIsShown ? "isShown" : "isHidden"}>
-          Salle de Métamorphose
-        </span>
-      </div>
+        <div className="Meta">
+          <Link to="/Lieux/5">
+            <input
+              type="button"
+              className="buttMeta"
+              onMouseEnter={() => setMetaIsShown(true)}
+              onMouseLeave={() => setMetaIsShown(false)}
+              onClick={() => {
+                handleCharacter(personages[5]);
+                handleLocation(lieu[5]);
+              }}
+            />
+          </Link>
+          <span className={metaIsShown ? "isShown" : "isHidden"}>
+            Salle de Métamorphose
+          </span>
+        </div>
 
-      <div className="Defense">
-        <Link to={`/Lieux/6`}>
-          <input
-            type="button"
-            className="buttDefense"
-            onMouseEnter={() => setDefenseIsShown(true)}
-            onMouseLeave={() => setDefenseIsShown(false)}
-            onClick={() => { handleCharacter(personages[2]); handleLocation(lieu[6]); }}
-          /></Link>
-        <p className={defenseIsShown ? "isShown" : "isHidden"}>
-          Salle de Défense <br />
-          contre les Forces du Mal
-        </p>
-      </div>
+        <div className="Defense">
+          <Link to="/Lieux/6">
+            <input
+              type="button"
+              className="buttDefense"
+              onMouseEnter={() => setDefenseIsShown(true)}
+              onMouseLeave={() => setDefenseIsShown(false)}
+              onClick={() => {
+                handleCharacter(personages[2]);
+                handleLocation(lieu[6]);
+              }}
+            />
+          </Link>
+          <p className={defenseIsShown ? "isShown" : "isHidden"}>
+            Salle de Défense <br />
+            contre les Forces du Mal
+          </p>
+        </div>
 
-      <div className="Foret">
-        <Link to={`/Lieux/7`}>
-          <input
-            type="button"
-            className="buttForet"
-            onMouseEnter={() => setForetIsShown(true)}
-            onMouseLeave={() => setForetIsShown(false)}
-            onClick={() => { handleLocation(lieu[7]); }}
-          /></Link>
-        <span className={foretIsShown ? "isShown" : "isHidden"}>
-          Fôret Interdite
-        </span>
-      </div>
+        <div className="Foret">
+          <Link to="/Lieux/7">
+            <input
+              type="button"
+              className="buttForet"
+              onMouseEnter={() => setForetIsShown(true)}
+              onMouseLeave={() => setForetIsShown(false)}
+              onClick={() => {
+                handleLocation(lieu[7]);
+              }}
+            />
+          </Link>
+          <span className={foretIsShown ? "isShown" : "isHidden"}>
+            Fôret Interdite
+          </span>
+        </div>
 
-      <div className="Dev">
-        <Link to={`/Lieux/8`}>
-          <input
-            type="button"
-            className="buttDev"
-            onMouseEnter={() => setDevIsShown(true)}
-            onMouseLeave={() => setDevIsShown(false)}
-            onClick={() => { handleLocation(lieu[8]); }}
-          /></Link>
-        <span className={devIsShown ? "isShown" : "isHidden"}>Dev au lard</span>
-      </div>
+        <div className="Dev">
+          <Link to="/Lieux/8">
+            <input
+              type="button"
+              className="buttDev"
+              onMouseEnter={() => setDevIsShown(true)}
+              onMouseLeave={() => setDevIsShown(false)}
+              onClick={() => {
+                handleLocation(lieu[8]);
+              }}
+            />
+          </Link>
+          <span className={devIsShown ? "isShown" : "isHidden"}>
+            Dev au lard
+          </span>
+        </div>
 
-      <div className="Quidditch">
-        <Link to={`/Lieux/9`}>
-          <input
-            type="button"
-            className="buttQuidditch"
-            onMouseEnter={() => setQuidditchIsShown(true)}
-            onMouseLeave={() => setQuidditchIsShown(false)}
-            onClick={() => { handleLocation(lieu[9]); }}
-          /></Link>
-        <span className={quidditchIsShown ? "isShown" : "isHidden"}>
-          Terrain de Quidditch
-        </span>
-      </div>
+        <div className="Quidditch">
+          <Link to="/Lieux/9">
+            <input
+              type="button"
+              className="buttQuidditch"
+              onMouseEnter={() => setQuidditchIsShown(true)}
+              onMouseLeave={() => setQuidditchIsShown(false)}
+              onClick={() => {
+                handleLocation(lieu[9]);
+              }}
+            />
+          </Link>
+          <span className={quidditchIsShown ? "isShown" : "isHidden"}>
+            Terrain de Quidditch
+          </span>
+        </div>
 
-      <div className="Saule">
-        <Link to={`/Lieux/10`}>
-          <input
-            type="button"
-            className="buttSaule"
-            onMouseEnter={() => setSauleIsShown(true)}
-            onMouseLeave={() => setSauleIsShown(false)}
-            onClick={() => { handleLocation(lieu[10]); }}
-          /></Link>
-        <span className={sauleIsShown ? "isShown" : "isHidden"}>
-          Saule Cogneur
-        </span>
-      </div>
+        <div className="Saule">
+          <Link to="/Lieux/10">
+            <input
+              type="button"
+              className="buttSaule"
+              onMouseEnter={() => setSauleIsShown(true)}
+              onMouseLeave={() => setSauleIsShown(false)}
+              onClick={() => {
+                handleLocation(lieu[10]);
+              }}
+            />
+          </Link>
+          <span className={sauleIsShown ? "isShown" : "isHidden"}>
+            Saule Cogneur
+          </span>
+        </div>
       </div>
     </>
   );
