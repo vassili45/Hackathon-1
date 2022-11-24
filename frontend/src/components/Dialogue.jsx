55232d31eb2ee75316b7a4e85a954184901c2f73
Amina HAKIMI <amina.hakimi86@gmail.com>
2022-11-24 14:56:25 +0100

    Ajout du composant Dialogue

---
+++ frontend/src/components/Dialogue.jsx
@@ -0,0 +1,15 @@
+
+function Dialogue () {
+    return (
+        <div>
+         <div className="card">
+            <img></img>
+            <h3></h3>
+            <p></p>
+         </div>
+
+        </div>
+    )
+}
+
+export default Dialogue;
